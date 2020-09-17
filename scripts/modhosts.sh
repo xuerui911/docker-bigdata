e28c28f87ca93cd1880b40604fa16461883312da
xuerui911 <46309554+xuerui911@users.noreply.github.com>
2020-09-17 16:34:07 +0800

    Create modhosts.sh

---
+++ scripts/modhosts.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+echo "192.168.88.101 hadoop101"  >> /etc/hosts
+echo "192.168.88.102 hadoop102"  >> /etc/hosts
+echo "192.168.88.103 hadoop103"  >> /etc/hosts
+echo "192.168.88.104 hadoop104"  >> /etc/hosts
+echo "192.168.88.105 hadoop105"  >> /etc/hosts
+/bin/sh
